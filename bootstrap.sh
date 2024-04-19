75b2f1d11d99782729eaa62610c83fa710c5e415
Peter Fors <peter.fors@mindkiller.com>
2024-04-19 15:06:06 +0200

    Fixed bootstrap.sh, and .gitignore was too greedy, ignored all 'include' directories at all levels.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "glfw/.git" ]; then
+if [ ! -f "loader/glfw/.git" ]; then
     echo "ERROR: Submodule 'glfw' not properly initialized. Please run 'git submodule update --init --recursive' before running this script."
     exit 1
 fi
@@ -21,10 +21,11 @@ build_linux() {
     echo "Building GLFW for Linux..."
     pushd loader/glfw || { echo "Failed to enter GLFW directory, have you checked out the submodules?"; exit 1; }
     mkdir -p build
-    cd build || { echo "Failed to enter build directory"; exit 1; }
+    pushd build || { echo "Failed to enter build directory"; exit 1; }
     cmake .. -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
     make -j"$(nproc)" || { echo "Failed to build GLFW for Linux"; exit 1; }
     mv src/*.a "../../lib/linux64" || { echo "Failed to move GLFW library files"; exit 1; }
+    popd || { echo "Failed to return to the original directory"; exit 1; }
     rm -rf build
     popd || { echo "Failed to return to the original directory"; exit 1; }
     echo "GLFW built for Linux successfully."
@@ -35,15 +36,24 @@ build_windows() {
     echo "Building GLFW for Windows with MinGW..."
     pushd loader/glfw || { echo "Failed to enter GLFW directory, have you checked out the submodules?"; exit 1; }
     mkdir -p build
-    cd build || { echo "Failed to enter build directory"; exit 1; }
+    pushd build || { echo "Failed to enter build directory"; exit 1; }
     cmake .. -DCMAKE_TOOLCHAIN_FILE=../CMake/x86_64-w64-mingw32.cmake -DBUILD_SHARED_LIBS=OFF -DGLFW_BUILD_DOCS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF
     make -j"$(nproc)" || { echo "Failed to build GLFW for Windows with MinGW"; exit 1; }
     mv src/*.a "../../lib/win32" || { echo "Failed to move GLFW library files"; exit 1; }
     popd || { echo "Failed to return to the original directory"; exit 1; }
     rm -rf build
+    popd || { echo "Failed to return to the original directory"; exit 1; }
     echo "GLFW built for Windows with MinGW successfully."
+}
+
+# Function to copy include files
+copy_include_files() {
+    echo "Copying include files..."
+    cp -r loader/glfw/include/* loader/include || { echo "Failed to copy include directory"; exit 1; }
+    echo "Include files copied successfully."
 }
 
 # Main script
 build_linux
 build_windows
+copy_include_files
