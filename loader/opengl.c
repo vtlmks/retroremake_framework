2f5bb5bd53591cd766dcc4861c24638809b5d72b
Peter Fors <peter.fors@mindkiller.com>
2024-04-30 16:55:55 +0200

    Remove some superfluous errorchecks.

--- loader/opengl.c
+++ loader/opengl.c
@@ -82,7 +82,7 @@ struct opengl opengl;
 #if defined(_WIN32)
 void *get_any_gl_address(const char *name) {
 	void *p = (void *)wglGetProcAddress(name);
-	if(p == 0 || (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) || (p == (void*)-1)) {
+	if(!p) {
 		HMODULE module = LoadLibraryA("opengl32.dll");
 		p = (void *)GetProcAddress(module, name);
 	}
